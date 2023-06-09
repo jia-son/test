03c351626a3bba7bf45132f3e173823a05d0ce14
jia-son <116625758+jia-son@users.noreply.github.com>
2023-06-09 23:57:40 +0900

    axios header 열어주기

--- src/main/java/com/connectiontest/test/configuration/WebConfig.java
+++ src/main/java/com/connectiontest/test/configuration/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS", "DELETE");
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "OPTIONS", "DELETE")
+                .exposedHeaders("Authorization", "RefreshToken");
     }
 }
